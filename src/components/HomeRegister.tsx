65f6f9f553e14fe14106a33ba6f2a4d798b4b3b3
Jeremy Zongker <jeremy@trilitech.com>
2021-02-03 23:27:46 -0600

    Fixed compilation and registration errors

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ApiHelper, RegisterInterface, LoginResponseInterface, ErrorMessages, EnvironmentHelper } from ".";
+import { ApiHelper, RegisterInterface, ErrorMessages, EnvironmentHelper } from ".";
 import { Row, Col, Container, Button } from "react-bootstrap"
 
 export const HomeRegister: React.FC = () => {
 
-    const [register, setRegister] = React.useState<RegisterInterface>({ churchName: "", displayName: "", password: "", email: "" });
+    const [register, setRegister] = React.useState<RegisterInterface>({ churchName: "", displayName: "", password: "", email: "", subDomain: "" });
     const [processing, setProcessing] = React.useState(false);
     const [firstName, setFirstName] = React.useState("");
     const [lastName, setLastName] = React.useState("");
@@ -18,6 +18,7 @@ export const HomeRegister: React.FC = () => {
         if (register.churchName === "") errors.push("Please enter your church name.")
         if (firstName === "") errors.push("Please enter your first name.")
         if (lastName === "") errors.push("Please enter your last name.")
+        if (register.subDomain === "") errors.push("Please select a subdomain for your church.")
         if (register.password === "") errors.push("Please enter a password.");
         else if (register.password.length < 6) errors.push("Passwords must be at least 6 characters.");
         if (register.email === "") errors.push("Please enter your email address.");
@@ -34,21 +35,9 @@ export const HomeRegister: React.FC = () => {
         if (validate()) {
             setProcessing(true);
             btn.innerHTML = "Registering. Please wait...";
-            const loginResp = await createAccess();
-            if (loginResp != null) {
-                btn.innerHTML = "Configuring...";
-                const promises: Promise<any>[] = [];
-                promises.push(ApiHelper.post("/churches/init", { user: loginResp.user }, "MembershipApi"));
-                promises.push(ApiHelper.post("/churches/init", [], "AttendanceApi"));
-                promises.push(ApiHelper.post("/churches/init", [], "GivingApi"));
-                let responses = await Promise.all(promises);
-
-                var errors: string[] = undefined;
-                responses.forEach(resp => { if (resp.errors !== undefined) errors = resp.errors; });
-                if (errors !== undefined) { setErrors(errors); return 0; }
-                else setRedirectUrl(EnvironmentHelper.AppUrl);
-
-            }
+            const loginResp = await ApiHelper.postAnonymous("/churches/register", register, "AccessApi");
+            if (loginResp.errors !== undefined) { setErrors(errors); return 0; }
+            else setRedirectUrl(EnvironmentHelper.AppUrl);
         }
 
         btn.innerHTML = "Register"
@@ -57,33 +46,6 @@ export const HomeRegister: React.FC = () => {
 
     }
 
-
-    const createAccess = async () => {
-        register.displayName = firstName + " " + lastName;
-
-        var resp: LoginResponseInterface = await ApiHelper.postAnonymous("/churches/register", register, "AccessApi");
-        if (resp.errors !== undefined) { setErrors(resp.errors); return null; }
-        else {
-            const church = resp.churches[0];
-            church.apis.forEach(api => { ApiHelper.setPermissions(api.keyName, api.jwt, api.permissions) })
-
-
-            resp = await ApiHelper.post("/churchApps/register", { appName: "ChurchApps" }, "AccessApi");
-            const churches = resp.churches[0]
-            if (resp.errors !== undefined) { setErrors(resp.errors); return null; }
-            else {
-                churches.apis.forEach(api => {
-                    console.log("SETTING PERMISSIONS");
-                    console.log(api.keyName);
-                    console.log(api.jwt);
-                    console.log(api.permissions);
-                    ApiHelper.setPermissions(api.keyName, api.jwt, api.permissions)
-                })
-                console.log(resp);
-                return resp;
-            }
-        }
-    }
 
     const getProcessing = () => {
         if (!processing) return null;
@@ -97,6 +59,7 @@ export const HomeRegister: React.FC = () => {
             case "churchName": r.churchName = val; break;
             case "firstName": setFirstName(val); break;
             case "lastName": setLastName(val); break;
+            case "subDomain": r.subDomain = val.toLowerCase().replaceAll(/[^a-z0-9]/ig, ""); break;
             case "email": r.email = val; break;
             case "password": r.password = val; break;
         }
@@ -113,22 +76,31 @@ export const HomeRegister: React.FC = () => {
                         <Col lg={6} className="d-none d-lg-block" ><img src="/images/home/register.png" alt="register" className="img-fluid" /></Col>
                         <Col lg={6}>
                             <div className="title"><span>Sign up for ChurchApps</span></div>
-                            <h2>Register for a Free Account</h2>
-                            <p>You'll be up and running in less than a minute.</p>
+                            <h2>Register Your Church</h2>
 
                             {getProcessing()}
                             <ErrorMessages errors={errors} />
                             <div className="form-group">
                                 <input type="text" className="form-control" placeholder="Church Name" name="churchName" value={register.churchName} onChange={handleChange} />
                             </div>
-
                             <div className="form-group">
-                                <input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
+                                <div className="input-group">
+                                    <input type="text" name="subDomain" className="form-control" placeholder="yourchurch" value={register.subDomain} onChange={handleChange} />
+                                    <div className="input-group-append"><span className="input-group-text">.churchapps.org</span></div>
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
-                            </div>
-
+                            <Row>
+                                <Col>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
+                                    </div>
+                                </Col>
+                            </Row>
 
                             <div className="form-group">
                                 <input type="text" className="form-control" placeholder="Email" name="email" value={register.email} onChange={handleChange} />
@@ -136,7 +108,7 @@ export const HomeRegister: React.FC = () => {
                             <div className="form-group">
                                 <input type="password" className="form-control" placeholder="Password" name="password" value={register.password} onChange={handleChange} />
                             </div>
-                            <Button variant="success" block onClick={handleRegister}>Get Started for Free</Button>
+                            <Button variant="success" block onClick={handleRegister}>Register for Free</Button>
 
 
                         </Col>
