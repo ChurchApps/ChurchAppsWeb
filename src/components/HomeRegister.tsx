cb6f2743fa45baca6b253730cbae45188032212c
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-06-09 22:12:12 +0530

    refactor: lint all files

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -4,159 +4,157 @@ import { Row, Col, Container, Button } from "react-bootstrap"
 
 export const HomeRegister: React.FC = () => {
 
-    const [register, setRegister] = React.useState<RegisterInterface>({ churchName: "", displayName: "", password: "", email: "", subDomain: "" });
-    const [processing, setProcessing] = React.useState(false);
-    const [firstName, setFirstName] = React.useState("");
-    const [lastName, setLastName] = React.useState("");
-    const [errors, setErrors] = React.useState<string[]>([]);
-    const [redirectUrl, setRedirectUrl] = React.useState("");
-    const [infoMessage, setInfoMessage] = React.useState([]);
+  const [register, setRegister] = React.useState<RegisterInterface>({ churchName: "", displayName: "", password: "", email: "", subDomain: "" });
+  const [processing, setProcessing] = React.useState(false);
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [errors, setErrors] = React.useState<string[]>([]);
+  const [redirectUrl, setRedirectUrl] = React.useState("");
+  const [infoMessage, setInfoMessage] = React.useState([]);
 
-    const validateEmail = (email: string) => { return (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(email)); }
+  const validateEmail = (email: string) => (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(email))
 
-    const validate = () => {
-        var errors: string[] = [];
-        if (register.churchName === "") errors.push("Please enter your church name.")
-        if (firstName === "") errors.push("Please enter your first name.")
-        if (lastName === "") errors.push("Please enter your last name.")
-        if (register.subDomain === "") errors.push("Please select a subdomain for your church.")
-        if (register.password === "") errors.push("Please enter a password.");
-        else if (register.password.length < 6) errors.push("Passwords must be at least 6 characters.");
-        if (register.email === "") errors.push("Please enter your email address.");
-        else if (!validateEmail(register.email)) errors.push("Please enter a valid email address");
-        setErrors(errors);
-        return errors.length === 0;
-    }
+  const validate = () => {
+    let errors: string[] = [];
+    if (register.churchName === "") errors.push("Please enter your church name.")
+    if (firstName === "") errors.push("Please enter your first name.")
+    if (lastName === "") errors.push("Please enter your last name.")
+    if (register.subDomain === "") errors.push("Please select a subdomain for your church.")
+    if (register.password === "") errors.push("Please enter a password.");
+    else if (register.password.length < 6) errors.push("Passwords must be at least 6 characters.");
+    if (register.email === "") errors.push("Please enter your email address.");
+    else if (!validateEmail(register.email)) errors.push("Please enter a valid email address");
+    setErrors(errors);
+    return errors.length === 0;
+  }
 
-    const handleRegister = async (e: React.MouseEvent) => {
-        e.preventDefault();
-        const btn = e.currentTarget;
-        btn.innerHTML = "Validating..."
-        btn.setAttribute("disabled", "disabled");
-        if (validate()) {
-            setProcessing(true);
-            btn.innerHTML = "Registering. Please wait...";
-            // check if user already exist and if so, return user's associated churches
-            const verifyResponse = await ApiHelper.postAnonymous("/users/verifyCredentials", {email: register.email, password: register.password}, "AccessApi");
-            if (verifyResponse.errors !== undefined) {
-                setErrors(verifyResponse.errors);
-                
-                btn.innerHTML = "Register"
-                btn.removeAttribute("disabled");
-                setProcessing(false);
-                return;
-            }
-            if (verifyResponse.churches !== undefined) {
-                const churchNames = verifyResponse.churches.map((e: any) => <b>{e}</b>);
-                const newChurchMessage = <>Would you like to register a new church of '<b>{register.churchName}</b>'?</>; 
-                setInfoMessage(["You are already associated with the following churches:", ...churchNames, newChurchMessage]);
-                return;
-            }
-
-            await registerChurch();
-        }
+  const handleRegister = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    const btn = e.currentTarget;
+    btn.innerHTML = "Validating..."
+    btn.setAttribute("disabled", "disabled");
+    if (validate()) {
+      setProcessing(true);
+      btn.innerHTML = "Registering. Please wait...";
+      // check if user already exist and if so, return user's associated churches
+      const verifyResponse = await ApiHelper.postAnonymous("/users/verifyCredentials", {email: register.email, password: register.password}, "AccessApi");
+      if (verifyResponse.errors !== undefined) {
+        setErrors(verifyResponse.errors);
 
         btn.innerHTML = "Register"
         btn.removeAttribute("disabled");
         setProcessing(false);
+        return;
+      }
+      if (verifyResponse.churches !== undefined) {
+        const churchNames = verifyResponse.churches.map((e: any) => <b>{e}</b>);
+        const newChurchMessage = <>Would you like to register a new church of '<b>{register.churchName}</b>'?</>;
+        setInfoMessage(["You are already associated with the following churches:", ...churchNames, newChurchMessage]);
+        return;
+      }
 
+      await registerChurch();
     }
 
-    const registerChurch = async () => {
-        register.displayName = firstName + " " + lastName;
-        const loginResp = await ApiHelper.postAnonymous("/churches/register", register, "AccessApi");
-        if (loginResp.errors !== undefined) { setErrors(loginResp.errors); setInfoMessage([]) }
-        else setRedirectUrl(EnvironmentHelper.AppUrl);
+    btn.innerHTML = "Register"
+    btn.removeAttribute("disabled");
+    setProcessing(false);
+
+  }
+
+  const registerChurch = async () => {
+    register.displayName = firstName + " " + lastName;
+    const loginResp = await ApiHelper.postAnonymous("/churches/register", register, "AccessApi");
+    if (loginResp.errors !== undefined) { setErrors(loginResp.errors); setInfoMessage([]) }
+    else setRedirectUrl(EnvironmentHelper.AppUrl);
+  }
+
+  const getProcessing = () => {
+    if (!processing) return null;
+    else return <div className="alert alert-info" role="alert"><b>Registering...</b> Please wait.  This will take a few seconds.</div>
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInfoMessage([])
+    const val = e.currentTarget.value;
+    let r = { ...register };
+    switch (e.currentTarget.name) {
+    case "churchName": r.churchName = val; break;
+    case "firstName": setFirstName(val); break;
+    case "lastName": setLastName(val); break;
+    case "subDomain": r.subDomain = val.toLowerCase().replaceAll(/[^a-z0-9]/ig, ""); break;
+    case "email": r.email = val; break;
+    case "password": r.password = val; break;
     }
+    setRegister(r);
+  }
 
+  const handleNo = () => {
+    setInfoMessage([])
+    setProcessing(false)
+  }
 
-    const getProcessing = () => {
-        if (!processing) return null;
-        else return <div className="alert alert-info" role="alert"><b>Registering...</b> Please wait.  This will take a few seconds.</div>
-    }
+  if (redirectUrl === "") {
+    return (
+      <div className="homeSection" id="registerSection">
+        <Container>
+          <div id="register"></div>
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInfoMessage([])
-        const val = e.currentTarget.value;
-        var r = { ...register };
-        switch (e.currentTarget.name) {
-            case "churchName": r.churchName = val; break;
-            case "firstName": setFirstName(val); break;
-            case "lastName": setLastName(val); break;
-            case "subDomain": r.subDomain = val.toLowerCase().replaceAll(/[^a-z0-9]/ig, ""); break;
-            case "email": r.email = val; break;
-            case "password": r.password = val; break;
-        }
-        setRegister(r);
-    }
+          <Row>
+            <Col lg={6} className="d-none d-lg-block"><img src="/images/home/register.png" alt="register" className="img-fluid" /></Col>
+            <Col lg={6}>
+              <div className="title"><span>Sign up for ChurchApps</span></div>
+              <h2>Register Your Church</h2>
 
-    const handleNo = () => {
-        setInfoMessage([])
-        setProcessing(false)
-    }
+              {getProcessing()}
+              <ErrorMessages errors={errors} />
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Church Name" name="churchName" value={register.churchName} onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <input type="text" name="subDomain" className="form-control" placeholder="yourchurch" value={register.subDomain} onChange={handleChange} />
+                  <div className="input-group-append"><span className="input-group-text">.churchapps.org</span></div>
+                </div>
+              </div>
+              <Row>
+                <Col>
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
+                  </div>
+                </Col>
+              </Row>
 
-    if (redirectUrl === "") {
-        return (
-            <div className="homeSection" id="registerSection">
-                <Container>
-                    <div id="register"></div>
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Email" name="email" value={register.email} onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control" placeholder="Password" name="password" value={register.password} onChange={handleChange} />
+              </div>
+              <ErrorMessages errors={infoMessage} />
+              {
+                infoMessage.length > 0 && (
+                  <Row className="mb-3">
+                    <Col><Button variant="danger" block onClick={handleNo}>No</Button></Col>
+                    <Col><Button variant="success" block onClick={registerChurch}>Yes</Button></Col>
+                  </Row>
+                )
+              }
 
-                    <Row>
-                        <Col lg={6} className="d-none d-lg-block" ><img src="/images/home/register.png" alt="register" className="img-fluid" /></Col>
-                        <Col lg={6}>
-                            <div className="title"><span>Sign up for ChurchApps</span></div>
-                            <h2>Register Your Church</h2>
+              { infoMessage.length === 0 && <Button variant="success" block onClick={handleRegister}>Register for Free</Button>}
 
-                            {getProcessing()}
-                            <ErrorMessages errors={errors} />
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Church Name" name="churchName" value={register.churchName} onChange={handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <input type="text" name="subDomain" className="form-control" placeholder="yourchurch" value={register.subDomain} onChange={handleChange} />
-                                    <div className="input-group-append"><span className="input-group-text">.churchapps.org</span></div>
-                                </div>
-                            </div>
-                            <Row>
-                                <Col>
-                                    <div className="form-group">
-                                        <input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
-                                    </div>
-                                </Col>
-                                <Col>
-                                    <div className="form-group">
-                                        <input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Email" name="email" value={register.email} onChange={handleChange} />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Password" name="password" value={register.password} onChange={handleChange} />
-                            </div>
-                            <ErrorMessages errors={infoMessage} />
-                            {
-                                infoMessage.length > 0 && (
-                                <Row className="mb-3">
-                                    <Col><Button variant="danger" block onClick={handleNo}>No</Button></Col>
-                                    <Col><Button variant="success" block onClick={registerChurch}>Yes</Button></Col>
-                                </Row>
-                                )
-                            }
-
-                             { infoMessage.length === 0 && <Button variant="success" block onClick={handleRegister}>Register for Free</Button>}
-
-
-                        </Col>
-                    </Row>
-                </Container>
-            </div >
-        );
-    } else {
-        window.location.href = redirectUrl;
-        return (<></>);
-    }
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  } else {
+    window.location.href = redirectUrl;
+    return (<></>);
+  }
 }
--- src/helpers/EnvironmentHelper.ts
+++ src/helpers/EnvironmentHelper.ts
@@ -6,34 +6,34 @@ export class EnvironmentHelper {
     static GoogleAnalyticsTag = "";
 
     static init = () => {
-        switch (process.env.REACT_APP_STAGE) {
-            case "staging": EnvironmentHelper.initStaging(); break;
-            case "prod": EnvironmentHelper.initProd(); break;
-            default: EnvironmentHelper.initDev(); break;
-        }
-        ApiHelper.apiConfigs = [
-            { keyName: "AccessApi", url: EnvironmentHelper.AccessApi, jwt: "", permisssions: [] },
-        ];
+      switch (process.env.REACT_APP_STAGE) {
+      case "staging": EnvironmentHelper.initStaging(); break;
+      case "prod": EnvironmentHelper.initProd(); break;
+      default: EnvironmentHelper.initDev(); break;
+      }
+      ApiHelper.apiConfigs = [
+        { keyName: "AccessApi", url: EnvironmentHelper.AccessApi, jwt: "", permisssions: [] },
+      ];
     }
 
     static initDev = () => {
-        EnvironmentHelper.AccessApi = process.env.REACT_APP_ACCESS_API || "";
-        EnvironmentHelper.GoogleAnalyticsTag = process.env.REACT_APP_GOOGLE_ANALYTICS || "";
-        EnvironmentHelper.AppUrl = process.env.REACT_APP_ACCOUNTS_APP_URL || ""
+      EnvironmentHelper.AccessApi = process.env.REACT_APP_ACCESS_API || "";
+      EnvironmentHelper.GoogleAnalyticsTag = process.env.REACT_APP_GOOGLE_ANALYTICS || "";
+      EnvironmentHelper.AppUrl = process.env.REACT_APP_ACCOUNTS_APP_URL || ""
     }
 
     //NOTE: None of these values are secret.
     static initStaging = () => {
-        EnvironmentHelper.AccessApi = "https://accessapi.staging.churchapps.org";
-        EnvironmentHelper.AppUrl = "https://accounts.staging.churchapps.org"
-        EnvironmentHelper.GoogleAnalyticsTag = "";
+      EnvironmentHelper.AccessApi = "https://accessapi.staging.churchapps.org";
+      EnvironmentHelper.AppUrl = "https://accounts.staging.churchapps.org"
+      EnvironmentHelper.GoogleAnalyticsTag = "";
     }
 
     //NOTE: None of these values are secret.
     static initProd = () => {
-        EnvironmentHelper.AccessApi = "https://accessapi.churchapps.org";
-        EnvironmentHelper.AppUrl = "https://accounts.churchapps.org"
-        EnvironmentHelper.GoogleAnalyticsTag = "UA-164774603-6";
+      EnvironmentHelper.AccessApi = "https://accessapi.churchapps.org";
+      EnvironmentHelper.AppUrl = "https://accounts.churchapps.org"
+      EnvironmentHelper.GoogleAnalyticsTag = "UA-164774603-6";
     }
 
 }
