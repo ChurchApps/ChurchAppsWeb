76d1816e50cfabaa609b45d42b12f36ecfd941cc
Jeremy Zongker <jeremy@trilitech.com>
2021-08-23 19:22:50 +0000

    Removed unused component.

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,6 @@ import "./Home.css";
 import { Footer, Header } from "./components"
 import { HomeRegister } from "./components/HomeRegister"
 import { HomeHero } from "./components/HomeHero"
-import { Overview } from "./components/Overview"
 import { AboutLessons } from "./components/AboutLessons"
 import { AboutChums } from "./components/AboutChums"
 import { AboutStreamingLive } from "./components/AboutStreamingLive"
