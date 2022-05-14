bf6a4d15f419a4348e14ec7a43ef5956c3cefdbf
Jeremy Zongker <jeremy@trilitech.com>
2022-05-14 14:24:22 -0500

    Added video to home page

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -8,6 +8,7 @@ export const HomeHero: React.FC = () => (
         <Col lg={{ span: 8, offset: 2 }} className="text-center">
           <h1>Completely <span>Free, Open-Source</span><br />Apps for Churches.</h1>
           <br />
+          <iframe style={{ border: 0, marginBottom: 20 }} width="560" height="315" src="https://www.youtube.com/embed/GjfACqs0xA8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
           <a href="/contribute" className="btn btn-info">Lean How You Can Get Involved</a>
         </Col>
       </Row>
--- src/environment/EnvironmentPage.tsx
+++ src/environment/EnvironmentPage.tsx
@@ -37,7 +37,6 @@ export const EnvironmentPage = () => (
           </Col>
         </Row>
 
-
       </Container>
     </div>
     <ApiProjects />
