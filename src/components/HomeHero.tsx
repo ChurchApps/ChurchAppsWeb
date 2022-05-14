766d1f5c2ce8cb344bf66d9fcf7efe70d1850f22
Jeremy Zongker <jeremy@trilitech.com>
2022-05-14 15:30:46 -0500

    Added debugger support

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -9,7 +9,7 @@ export const HomeHero: React.FC = () => (
           <h1>Completely <span>Free, Open-Source</span><br />Apps for Churches.</h1>
           <br />
           <iframe style={{ border: 0, marginBottom: 20 }} width="560" height="315" src="https://www.youtube.com/embed/GjfACqs0xA8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-          <a href="/contribute" className="btn btn-info">Lean How You Can Get Involved</a>
+          <a href="/contribute" className="btn btn-info btn-lg">Lean How You Can Get Involved</a>
         </Col>
       </Row>
     </Container>
