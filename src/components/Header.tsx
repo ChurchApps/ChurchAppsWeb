cceaa6e1792ba3d33d912e10f62391a353dc4818
PhotoPaul <PhotoPaul@gmail.com>
2023-01-10 20:15:08 +0200

    Updated login link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Container, AppBar, Stack, Box, Button } from "@mui/material";
-import { EnvironmentHelper } from "../helpers";
 
 export const Header: React.FC = () => (<>
   <AppBar id="navbar" position="fixed">
@@ -8,7 +7,7 @@ export const Header: React.FC = () => (<>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <a href="/" className="logo"><img src="/images/logo.png" alt="logo" /></a>
         <Box sx={{display: "flex", alignItems: "center", whiteSpace: "nowrap"}}>
-          <a href={EnvironmentHelper.AppUrl} className="link">Login</a>
+          <a href={"https://app.chums.org/login"} className="link">Login</a>
           <Button href="/#register" variant="contained" size="small" disableElevation color="success" sx={{ "&:focus": { outline: "none", color: "white" }, marginLeft: "15px" }}>Get Started</Button>
         </Box>
       </Stack>
