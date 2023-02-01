51ff368c4cb24556e931b1eb52d066b25afe1065
Jeremy Zongker <jeremy@trilitech.com>
2023-02-01 17:39:25 -0600

    Updated dependencies

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -16,13 +16,21 @@ export const Routing: React.FC = () => {
   }
   React.useEffect(() => { if (EnvironmentHelper.GoogleAnalyticsTag !== "") ReactGA.pageview(location.pathname + location.search); }, [location]);
 
+  const determineHome = () => {
+    let result = <Home />;
+    const host = window.location.hostname.toLocaleLowerCase();
+    console.log("Host is", host);
+    if (host.indexOf("chums.org") > -1) result = <ContributePage />
+    return result;
+  }
+
   return (
     <Routes>
       <Route path="/privacy" element={<Privacy />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/contribute/environment" element={<EnvironmentPage />} />
       <Route path="/contribute" element={<ContributePage />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={determineHome()} />
     </Routes>
   );
 }
