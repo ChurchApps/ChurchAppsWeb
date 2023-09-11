be0cf4363bf8bfb05a10eafcbd14f15097752d07
Jeremy Zongker <jeremy@zongker.net>
2023-09-11 09:35:58 -0500

    Fixed spelling error

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -9,7 +9,7 @@ export const HomeHero: React.FC = () => (
           <h1 style={{fontSize: "40px", lineHeight: 1.2, marginTop: 0}}>Completely <span>Free, Open-Source</span><br />Apps for Churches.</h1>
           <br />
           <iframe style={{ border: 0, marginBottom: 20, maxWidth: "100%" }} width="560" height="315" src="https://www.youtube.com/embed/GjfACqs0xA8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-          <Button href="/contribute" variant="contained" size="medium" disableElevation sx={{"&:focus": { outline: "none", color: "white"}, textTransform: "capitalize", fontSize: "20px"}}>Lean How You Can Get Involved</Button>
+          <Button href="/contribute" variant="contained" size="medium" disableElevation sx={{"&:focus": { outline: "none", color: "white"}, textTransform: "capitalize", fontSize: "20px"}}>Learn How You Can Get Involved</Button>
         </Grid>
       </Grid>
     </Container>
