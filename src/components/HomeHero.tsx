8574b8e8d85fd6660c4a9b624bf53812902b3b0f
Jeremy Zongker <jeremy@trilitech.com>
2023-12-12 20:54:47 -0600

    Update Home Video

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -8,7 +8,7 @@ export const HomeHero: React.FC = () => (
         <Grid item md={8} sx={{margin: "0 auto"}}>
           <h1 style={{fontSize: "40px", lineHeight: 1.2, marginTop: 0}}>Completely <span>Free, Open-Source</span><br />Apps for Churches.</h1>
           <br />
-          <iframe style={{ border: 0, marginBottom: 20, maxWidth: "100%" }} width="560" height="315" src="https://www.youtube.com/embed/GjfACqs0xA8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+          <iframe style={{ border: 0, marginBottom: 20, maxWidth: "100%" }} width="560" height="315" src="https://www.youtube.com/embed/ruExJ-WLFjU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
           <Button href="/contribute" variant="contained" size="medium" disableElevation sx={{"&:focus": { outline: "none", color: "white"}, textTransform: "capitalize", fontSize: "20px"}}>Learn How You Can Get Involved</Button>
         </Grid>
       </Grid>
